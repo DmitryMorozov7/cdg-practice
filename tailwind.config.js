a535fcd3f5abedf19254a31d693bed5f6811026a
Dmitry Morozov <dimon.morozov02@yandex.ru>
2023-07-06 14:02:36 +0300

    Added html structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ['./**/*.{html, css}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Monteserrat: ["Monteserrat", "sans-serif"],
+      },
+      colors: {
+        black: '#27272A',
+        greyDark: '#696969',
+        greyWhite: '#27272A',
+        white: '#FCFBFA',
+        lavender: '#7E3AF2',
+        red: '#E02424'
+      },
+      spacing: {
+        '1': '16',
+        '1,56': '25',
+        '1': '16',
+        '1': '16',
+        '1': '16',
+      },
+      backgroungImage: {
+        'send': "url('/img/Send.svg')"
+      }
+    },
   },
   plugins: [],
 }
